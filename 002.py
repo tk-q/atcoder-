9f984d0af5118143c3a423549e2e0b2eda6138f5
tk-q <103816066+tk-q@users.noreply.github.com>
2022-09-05 03:16:10 +0900

    Create 002.py

---
+++ 002.py
@@ -0,0 +1,32 @@
+#問題文url  https://atcoder.jp/contests/typical90/tasks/typical90_b
+
+N=int(input())
+import itertools
+count1=0
+count2=0
+M=[]
+M1=set()
+m=""
+co3=0
+Flag=True
+if N%2==0:
+    for k in range(2**N):
+        for l in range(N):
+            if ((k>>l)&1)==1:
+                m+=")"
+                count1+=1
+            else:
+                m+="("
+                count2+=1
+            if count2>count1 or count2>N//2 or count1>N//2:
+                count1=0
+                count2=0
+                m=""
+                Flag=False
+                break
+        if Flag==True:
+            print(m[::-1])
+            co3+=1
+        m=""
+        Flag=True
+
