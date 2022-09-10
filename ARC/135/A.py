3efdf7d22dd7944fd163821e39340c6eecbed48b
TK＠自然言語処理 <103816066+tk-q@users.noreply.github.com>
2022-09-11 02:01:38 +0900

    Create A.py

---
+++ ARC/135/A.py
@@ -0,0 +1,12 @@
+from functools import lru_cache
+X=int(input())
+
+while X>=5:
+    if X<=4:
+        break
+    a=x//2
+    b=x-a
+    x=(a1(a)*a1(b))%998244353
+    
+X=int(input())
+print(a1(X))
