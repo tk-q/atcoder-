fe78f841f6a06d56150f326501c24792ae6d98e9
TK＠自然言語処理 <103816066+tk-q@users.noreply.github.com>
2023-02-11 01:27:10 +0900

    Create 3.py
    パスグラフであるかを判定する。
    必要なこと
    頂点の数−１が辺の数　M =N-1
    グループが一つ　uf.group_count()==1
    辺が３以上でないようにする　d[u-1]>=3 or d[v-1]>=3

---
+++ ABC/287/3.py
@@ -0,0 +1,72 @@
+N,M=map(int,input().split())
+from collections import defaultdict
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+uf = UnionFind(N)
+from collections import defaultdict
+d = defaultdict(int)
+
+for i in range(M):
+    u,v=map(int,input().split())
+    d[u-1]+=1
+    d[v-1]+=1
+    if d[u-1]>=3 or d[v-1]>=3:
+        print("No")
+        exit()
+    if uf.same(u-1,v-1):
+        print("No")
+        exit()
+    uf.union(u-1,v-1)
+if M==N-1 and uf.group_count()==1:
+    print("Yes")
+else:
+    print("No")
+    
